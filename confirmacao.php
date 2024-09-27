b4fdeaf4732a29e2dd3fe28db5ed60466b0b7b81
giovannaseijass <ancilotoseijas@gmail.com>
2024-09-27 08:58:02 -0300

    Add files via upload

---
+++ confirmacao.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenDump</title>
+    <link rel="stylesheet" type="text/css" href="css/css.css"/>
+    <link rel="icon" type="image/x-icon" href="imgs/logov2.png">
+</head>
+<body>
+    <br> <br> <br> <br> <br> <br> <br> <br>
+    <div class="centroamarelo"></div>
+    <div class="centrorosa"></div>
+    <div class="proximocentro">
+        <p>Você é $usuario de $curso?</p>
+        <br> <br> 
+        <div class="botaos"> 
+            <a href="contagem.php">
+                <button type="input" class="opcao">
+                    Sim 
+                </button>
+            </a>
+            <a href="rm.php">
+                <button type="input" class="opcao">
+                    Não 
+                </button>
+            </a>
+        </div>
+        <br> 
+        <div class="logo">
+            <img src="imgs/loguinho.png" alt="logo">
+        </div>
+        <br>
+    </div>
+
+    </div>
+    <div class="folha1">
+    <img src="imgs/folhasBaixoDireita.png" alt="folhas debaixo">
+    </div>
+    <div class="folha2">
+    <img src="imgs/folhasBaixoEsquerda.png" alt="folhas esquerda">
+    </div>
+</body>
+</html>
